87e2a5631f4222c2acdabae7b43bde7af7474626
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2022-12-06 11:55:22 +0300

    two passing one failing

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+//Add your code here
+const url = ("http://localhost:3000/users")
+function submitData(name, email) {
+  return fetch(url,{
+    method: "POST",
+    headers: {
+      "Comtnent-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    }),
+  })
+  .then(response => { return response.json()})
+  .then((object) => {document.body.innerHTML = object["id"]})
+  .catch(error => {document.body.innerHTML = error.message;})
+}
